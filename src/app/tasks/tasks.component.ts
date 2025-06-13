3924e35050c9e3f628d7eaaa6c2e7a65f68a9bfa
Anil Kumar <anilkumar.dcttechnology@gmail.com>
2025-06-13 19:00:53 +0530

    get the understanding in how many ways we can inject services

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TmplAstBlockNode } from '@angular/compiler';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
+  providers: [TmplAstBlockNode], //This is 2nd way of provide the services to other component but the restriction is the services only available in this component and it's child component
 })
 export class TasksComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+//import { TasksService } from './app/tasks/tasks-list/tast.services';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+//other way of providing the services to the all component in our app is below we use (Provider:[]) in here add the services that are needed
+// bootstrapApplication(AppComponent,
+//     {providers: [TasksService]}
+
+// ).catch((err) => console.error(err));
